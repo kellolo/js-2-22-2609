27ad32695ea1537250c6de867a369b87e390122e
gruzinenko <63999763+gruzinenko@users.noreply.github.com>
2020-09-30 17:05:51 +0700

    lesson_1

---
+++ 3-STUDENTS/nikita_gruz/project/src/components/index.js
@@ -0,0 +1,12 @@
+import app from './app.js';
+import cart from './cart.js';
+import catalog from './catalog.js';
+import gallery from './gallery.js';
+
+
+export default () => {
+    app.init();
+    cart.init();
+    catalog.init();
+    gallery.init();
+}
