0e09b179ad99e7ef252fa6fb207d4d9ba6711fd4
skala_15 <idanilas@mail.ru>
2020-10-03 02:03:50 +0300

    Не доделал

--- 3-STUDENTS/ivan_dan/project/src/components/catalog.js
+++ 3-STUDENTS/ivan_dan/project/src/components/catalog.js
@@ -23,19 +23,19 @@ function createItemTemplate(item) {
 </div>`
 }
 
-
+/*
 export default {
     container: null,
     url: 'https://raw.githubusercontent.com/kellolo/static/master/JSON/catalog.json',
     items: [],
     basket: null,
-    init () {
+    init (basket) {
         this.container = document.querySelector('#catalog');
+        this.basket = basket; //ссылка на объект basket из файла cart.js
         this.getData(this.url)
             .then(items => {this.items = items})
             .finally(() => {
                 this._render();
-                this.basket = basket; //ссылка на объект basket из файла cart.js
                 this.handleActions();
             })
     },
@@ -65,6 +65,58 @@ export default {
         });
         this.container.innerHTML = htmlStr;
     }
+}*/
+
+
+export default class Catalog {
+    constructor(name) {
+        this.name = name;
+        this.container = null;
+        this.url = 'https://raw.githubusercontent.com/kellolo/static/master/JSON/catalog.json';
+        this.items = [];
+        this.basket = null;
+        this._init();
+        this._getData(url);
+        this._handleActions();
+        this._render();
+    }
+    _init (basket) {
+        this.container = document.querySelector('#catalog');
+        this.basket = basket; //ссылка на объект basket из файла cart.js
+        this.getData(this.url)
+            .then(items => {this.items = items})
+            .finally(() => {
+                this._render();
+                this.handleActions();
+            })
+    }
+    _getData(url) {
+        return fetch(url).then(data => data.json())
+    }
+    _handleActions() {
+        this.container.addEventListener('click', evt => {
+            if (evt.target.name == 'add') {
+                let datas = evt.target.dataset;
+
+                let newProd = {
+                    productId: datas.id,
+                    productPrice: +datas.price,
+                    productName: datas.name,
+                    productImg: datas.image
+                }
+
+                this.basket.add(newProd);
+            }
+        })
+    }
+    _render() {
+        let htmlStr = '';
+        this.items.forEach(item => {
+            htmlStr += createItemTemplate(item);
+        });
+        this.container.innerHTML = htmlStr;
+    }
+
 }
 
 
