e9affaf02b12fede0aabc444119d3227e888eaee
Igor <kpe4ets@gmail.com>
2020-10-02 14:40:37 +0300

    hw 2.2 - bothClasses

--- 3-STUDENTS/ipetrov/project/src/components/index.js
+++ 3-STUDENTS/ipetrov/project/src/components/index.js
@@ -1,8 +1,9 @@
 import Basket from './basket.js';
 import basket from './basket.js'
+import Catalog from './catalog.js';
 import catalog from './catalog.js'
 
 export default () => {
     let basket = new Basket();
-    catalog.init(basket);
+    let catalog = new Catalog(basket);
 }
