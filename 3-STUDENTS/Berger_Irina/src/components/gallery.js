725c9bea98e52ff978959e8a2a1379f09dfd0d3a
Irina <ioberger@ya.ru>
2020-10-07 12:07:29 +0300

    Finally I did my homework

--- 3-STUDENTS/Berger_Irina/src/components/gallery.js
+++ 3-STUDENTS/Berger_Irina/src/components/gallery.js
@@ -6,11 +6,11 @@ setTimeout(() => {
     let openModalWindow = document.querySelectorAll(".catalog__name");
 
     openModalWindow.forEach(el => {
-        el.addEventListener("click", function (event) {
+        el.addEventListener("click", function(event) {
             event.preventDefault();
             page.classList.toggle('blur');
             popup.classList.toggle("state-appear");
-        });   
+        });
     });
 
     closeModalWindow.addEventListener('click', evt => {
@@ -20,93 +20,93 @@ setTimeout(() => {
 }, 300);
 
 // Галлерея
-    let sliderCount = 0;
+let sliderCount = 0;
 
-    /* Слайдер вперед */
-    document.querySelector('#next').addEventListener('click', evt => {
-        getMainImgIndex();
-        gallery.main.setAttribute('src', gallery.items[sliderCount + 1]);
-        if (sliderCount >= gallery.items.length - 1) {
-            gallery.main.setAttribute('src', gallery.items[0]);
-        } 
-    });
-
-    /* Слайдер назад */
-    document.querySelector('#prev').addEventListener('click', evt => {
-        getMainImgIndex();
-        gallery.main.setAttribute('src', gallery.items[sliderCount - 1]);
-        if (sliderCount == 0) {
-            gallery.main.setAttribute('src', gallery.items[gallery.items.length - 1]);
-        } 
-    });
-
-    function getMainImgIndex() {
-        gallery.items.forEach((el, index) => {
-            if(gallery.main.src == el) {
-                return sliderCount = index;
-            }
-        });
+/* Слайдер вперед */
+document.querySelector('#next').addEventListener('click', evt => {
+    getMainImgIndex();
+    gallery.main.setAttribute('src', gallery.items[sliderCount + 1]);
+    if (sliderCount >= gallery.items.length - 1) {
+        gallery.main.setAttribute('src', gallery.items[0]);
     }
+});
 
-    
+/* Слайдер назад */
+document.querySelector('#prev').addEventListener('click', evt => {
+    getMainImgIndex();
+    gallery.main.setAttribute('src', gallery.items[sliderCount - 1]);
+    if (sliderCount == 0) {
+        gallery.main.setAttribute('src', gallery.items[gallery.items.length - 1]);
+    }
+});
 
-    let gallery = {
-        main: null,
-        carousel: null,
-        items: [],
-        baseUrl: 'https://raw.githubusercontent.com/Eliseev88/geekbrains/master/pr_omega/src/layout/catalog.json',
-        
-        init () {
-            this.main = document.querySelector('#main-pic');
-            this.carousel = document.querySelector('#carousel');
-            this.getImages();
-            setTimeout(() => {
-                this.render();
-                this.handleActions();
-            }, 200);
-        },
+function getMainImgIndex() {
+    gallery.items.forEach((el, index) => {
+        if (gallery.main.src == el) {
+            return sliderCount = index;
+        }
+    });
+}
 
-        getImages() {
-            fetch(this.baseUrl)
+
+
+let gallery = {
+    main: null,
+    carousel: null,
+    items: [],
+    baseUrl: 'https://raw.githubusercontent.com/Eliseev88/geekbrains/master/pr_omega/src/layout/catalog.json',
+
+    init() {
+        this.main = document.querySelector('#main-pic');
+        this.carousel = document.querySelector('#carousel');
+        this.getImages();
+        setTimeout(() => {
+            this.render();
+            this.handleActions();
+        }, 200);
+    },
+
+    getImages() {
+        fetch(this.baseUrl)
             .then(d => d.json())
             .then(items => {
-                this.items = items.map(el => el.productImg);    
+                this.items = items.map(el => el.productImg);
             });
-        },
-        handleActions() {
-            let currentUrlImg = this.main.src;
-            this.carousel.addEventListener('click', evt => {
-                if(evt.target.name == 'preview') {
-                    this.changeMainImg(evt.target.src);
-                    return currentUrlImg = evt.target.src;
-                }
-            });
-            this.carousel.addEventListener("mouseover", evt => {
-                if(evt.target.name == 'preview') {
-                    this.changeMainImg(evt.target.src);
-                } 
-            });
-            /* Если нужен возврат картники после отвода мыши */
-            // this.carousel.addEventListener('mouseout', evt => {
-            //     if(evt.target.name == 'preview') {
-            //         this.changeMainImg(currentUrlImg);
-            //     } else {
-            //         return;
-            //     }
-            // });
-        },
-        changeMainImg(url) {
-            this.main.src = url;
-        },
-        render() {
-            this.main.src = this.items[0];
-            let str = '';
-            this.items.forEach(imgUrl => {
-                str += `<img src="${imgUrl}" class="popup__preview" name="preview">`
-            })
-            this.carousel.innerHTML = str;
-        }
+    },
+    handleActions() {
+        let currentUrlImg = this.main.src;
+        this.carousel.addEventListener('click', evt => {
+            if (evt.target.name == 'preview') {
+                this.changeMainImg(evt.target.src);
+                return currentUrlImg = evt.target.src;
+            }
+        });
+        this.carousel.addEventListener("mouseover", evt => {
+            if (evt.target.name == 'preview') {
+                this.changeMainImg(evt.target.src);
+            }
+        });
+        /* Если нужен возврат картники после отвода мыши */
+        // this.carousel.addEventListener('mouseout', evt => {
+        //     if(evt.target.name == 'preview') {
+        //         this.changeMainImg(currentUrlImg);
+        //     } else {
+        //         return;
+        //     }
+        // });
+    },
+    changeMainImg(url) {
+        this.main.src = url;
+    },
+    render() {
+        this.main.src = this.items[0];
+        let str = '';
+        this.items.forEach(imgUrl => {
+            str += `<img src="${imgUrl}" class="popup__preview" name="preview">`
+        })
+        this.carousel.innerHTML = str;
     }
- gallery.init();
+}
+gallery.init();
 
-
+export default gallery;
