70fa1849e90ba01b08eea63d43950c15e156bc77
Nikita <nik-bogachev@mail.ru>
2020-10-12 11:33:33 +0500

    hw5

--- 3-STUDENTS/nikita_bogachev/project/src/components/index.js
+++ 3-STUDENTS/nikita_bogachev/project/src/components/index.js
@@ -1,9 +1,68 @@
-import Basket from './basket.js';
-import Catalog  from './catalog.js';
 import { displayMenu } from './app';
 
 export default () => {
-    let basket = new Basket();
-    let catalog = new Catalog(basket);
     displayMenu();
 }
+
+const { event } = require("jquery");
+
+const app = new Vue ({
+    el: '#app',
+    data: {
+        catalogUrl: 'https://raw.githubusercontent.com/kellolo/static/master/JSON/catalog.json',
+        basketUrl: 'https://raw.githubusercontent.com/kellolo/static/master/JSON/basket.json',
+        items: [],
+        basket: [],
+    },
+    methods: {
+        _get(url) {
+            return fetch(url).then(data => data.json())
+        },
+        _shownBasket () {
+            document.querySelector('#basket').classList.toggle('invisible')
+        },
+        add(product) {
+            this.shown == true
+            let find = this.basket.find(el => el.productId == product.productId);
+                if (!find) {
+                    this.basket.push(Object.assign(product, { amount: 1 }));
+                } else {
+                    find.amount++;
+                }
+        },
+        addInBasket(id){
+            let catalogItem = document.querySelector(`#${id}`);
+            let datas  = catalogItem.querySelector('button')
+            
+               let newProd = {
+                    productId: datas.dataset.id,
+                    productPrice: +datas.dataset.price,
+                    productName: datas.dataset.name,
+                    productImg: datas.dataset.image
+                }
+
+               this.add(newProd);
+        },
+        _remove(id) {
+            let find = this.basket.find(el => el.productId == id);
+            if (find.amount > 1) {
+                find.amount--;
+            } else {
+                this.basket.splice(this.items.indexOf(find), 1);
+            }
+        },
+        _sum(){
+            let totalSum = 0;
+            this.basket.forEach( el =>{totalSum+=el.productPrice*el.amount});
+            document.querySelector('#total-sum').innerText='$'+totalSum;
+        }
+        
+    },
+    mounted() {
+        this._get(this.catalogUrl)
+        .then(item => {this.items = item});
+        this._get(this.basketUrl)
+        .then(basket => {this.basket = basket.content});
+        this._sum()
+    }
+})
