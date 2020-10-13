b36c1074e3685e5ac2a4f3d23abc0d4d1404232c
Igor <kpe4ets@gmail.com>
2020-10-14 00:27:09 +0300

    step 2

--- 3-STUDENTS/ipetrov/project/src/index.js
+++ 3-STUDENTS/ipetrov/project/src/index.js
@@ -1,6 +1,46 @@
-import app from "./components"
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './layout/styles/style.css'
 
-app();
+const app = new Vue({
+    el: '#app',
+    data: {
+        catalogURL: 'https://raw.githubusercontent.com/kpe4et/static/master/JSON/catalog.json',
+        catalogItems: [],
+        basketURL: 'https://raw.githubusercontent.com/kpe4et/static/master/JSON/basket.json',
+        basketItems: [],
+        show: false,
+    },
+    methods: {
+        _get(url) {
+            return fetch(url).then(d => d.json())
+        },
+        add(item) {
+            basketItems += item;
+        },
+    },
+    mounted() {
+        this._get(this.catalogURL)
+        .then(items => 
+            {this.catalogItems = items; console.log(this.catalogItems)
+            })
+    }
+})
+
+
+
+// import app from "./components"
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import './layout/styles/style.css'
+
+// app();
+
+// let find = this.items.find(el => el.productId == product.productId);
+// if (!find) {
+//     this.items.push(Object.assign(product, { amount: 1 }));
+// } else {
+//     find.amount++;
+// }
+
+
