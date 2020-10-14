24d5c6a739a2448e9fc2cccbf80f662281b57e4c
EvgenZagg <65083598+EvgenZagg@users.noreply.github.com>
2020-10-14 15:06:41 +0300

    HM5

--- 3-STUDENTS/Zagorelsky_Evgeny/project/src/components/catalog.js
+++ 3-STUDENTS/Zagorelsky_Evgeny/project/src/components/catalog.js
@@ -1,27 +1,27 @@
 
-import List from './LIST.js'
+// import List from './LIST.js'
 
-export default class Catalog extends List {
-    constructor(basket, url = '/catalog.json', container = '#catalog') {
-        super(container, url, basket)
-    }
+// export default class Catalog extends List {
+//     constructor(basket, url = '/catalog.json', container = '#catalog') {
+//         super(container, url, basket)
+//     }
 
-    _handleActions() {
-        this.container.addEventListener('click', evt => {
-            if (evt.target.name == 'add' || evt.target.parentNode.name == 'add') {
-                let datas = evt.target.tagname == 'add' ? evt.target.dataset : evt.target.parentNode.dataset;
+//     _handleActions() {
+//         this.container.addEventListener('click', evt => {
+//             if (evt.target.name == 'add' || evt.target.parentNode.name == 'add') {
+//                 let datas = evt.target.tagname == 'add' ? evt.target.dataset : evt.target.parentNode.dataset;
 
-                let newProd = {
-                    productId: datas.id,
-                    productPrice: +datas.price,
-                    productName: datas.name,
-                    productImg: datas.image
-                }
+//                 let newProd = {
+//                     productId: datas.id,
+//                     productPrice: +datas.price,
+//                     productName: datas.name,
+//                     productImg: datas.image
+//                 }
 
-                this.basket.add(newProd);
-            }
-        })
-    }
-}
+//                 this.basket.add(newProd);
+//             }
+//         })
+//     }
+// }
 
 
