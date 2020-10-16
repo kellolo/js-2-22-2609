11311f33dc3d62f98effe09d54b2faea6e164534
KaterinaLove <KaterinaLove@users.noreply.github.com>
2020-10-16 20:44:48 +0300

    Lesson6

---
+++ 3-STUDENTS/sotnichenko_ekaterina/project/src/components/Catalog.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="catalog__main" id="catalog">
+      <ItemCatalog 
+        v-for="item of items" 
+        :key="item.productId" 
+        :item="item" 
+        @add="add(item)" />
+  </div>
+</template>
+
+<script>
+import ItemCatalog from './ItemCatalog'
+import Basket from './Basket'
+
+export default {
+    components: { ItemCatalog },
+    data() {
+        return {
+            items: [],
+            url: 'https://raw.githubusercontent.com/kellolo/static/master/JSON/catalog.json'
+        }
+    },
+    mounted() {
+        this.get(this.url).then(items => this.items = items)
+    },
+    methods: {
+        get(url) {
+            return fetch(url)
+                .then(data => data.json())
+        },
+    add(item) {
+            return console.log('я не знаю как отправить данные в корзину   ' + item.productId);
+        }
+    }
+}
+</script>
