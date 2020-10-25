8a6c31e950e7357a8ea5ce1e04c83cfb356e21ec
Timon4eg <62251008+Timon4eg@users.noreply.github.com>
2020-10-25 17:55:34 +0300

    routing 1
    делаем страницу views/Product.vue

--- 3-STUDENTS/lyubimov_artem/vue_cli_project/src/router/index.js
+++ 3-STUDENTS/lyubimov_artem/vue_cli_project/src/router/index.js
@@ -11,13 +11,25 @@ const routes = [
     component: Index
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/product',
+    name: 'Product',
+    component: () => import('../views/Product.vue')
+  },
+  {
+    path: '/checkout',
+    name: 'Checkout',
+    component: () => import('../views/Checkout.vue')
+  },
+  {
+    path: '/shoppingCart',
+    name: 'ShoppingCart',
+    component: () => import('../views/ShoppingCart.vue')
+  },
+  {
+    path: '/singlepage',
+    name: 'SinglePage',
+    component: () => import('../views/SinglePage.vue')
+  },
 ]
 
 const router = new VueRouter({
---
+++ 3-STUDENTS/lyubimov_artem/vue_cli_project/src/views/Product.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <HeaderProduct />
+        <div class="headerLine"></div>
+        <NavProduct />
+        <NewArrivalsBg />
+        <main>
+            <Navigation />
+            <ProductsFeaturesBg />
+        </main>
+        <SubscribeBg />
+        <FooterContainer />
+        <BgCopyrights />
+    </div>
+</template>
+
+<script>
+import HeaderProduct from '../components/blocks/product/HeaderProduct.vue';
+import NavProduct from '../components/blocks/product/NavProduct.vue';
+import NewArrivalsBg from '../components/blocks/product/NewArrivalsBg.vue';
+import FooterContainer from '../components/blocks/product/FooterContainer.vue';
+import BgCopyrights from '../components/blocks/product/BgCopyrights.vue';
+import Navigation from '../components/blocks/product/Navigation.vue';
+import ProductsFeaturesBg from '../components/blocks/product/ProductsFeaturesBg.vue';
+import SubscribeBg from '../components/blocks/product/SubscribeBg.vue';
+
+export default {
+    components: {
+        HeaderProduct,
+        NavProduct,
+        NewArrivalsBg,
+        FooterContainer,
+        BgCopyrights,
+        Navigation,
+        ProductsFeaturesBg,
+        SubscribeBg
+    }
+}
+</script>
