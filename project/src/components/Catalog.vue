1b9b7e613192bf5764f5c0c015a1422c7c48a49c
EvgenZagg <65083598+EvgenZagg@users.noreply.github.com>
2020-10-27 16:46:33 +0300

    Home work 8

---
+++ project/src/components/Catalog.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="catalog__main" id="catalog">
+      <Item v-for="item of catalog" :key="item.productId" :item="item" />
+  </div>
+</template>
+
+<script>
+import Item from './additional/Item.vue';
+import { mapActions, mapGetters } from 'vuex'
+export default {
+    components: { Item },
+    mounted() {
+           this.get_catalog_items()
+    },
+    methods: {
+        ...mapActions([
+            'get_catalog_items'
+        ]),
+    },
+    
+    computed: {
+            ...mapGetters([
+                'catalog'
+            ])
+        } 
+}
+
+
+
+</script>
+
+<style>
+
+</style>
---
+++ project/src/components/Nav.vue
@@ -0,0 +1,90 @@
+<template>
+        <nav class="nav center">
+        <ul class="menu">
+          <li class="menu__list">
+            <router-link to="/" class="menu__link active">Home</router-link>
+          </li>
+          <li class="menu__list">
+            <a href="product.html" class="menu__link">Man</a>
+            <div class="mega_menu">
+              <div class="mega_menu__column">
+                <MenuWoman />
+              </div>
+              <div class="mega_menu__column">
+                <MenuWomanMini />
+                <h6 class="mega_menu__title mega_menu__title--middle">Women</h6>
+                <ul class="mega_menu__ul">
+                  <li class="mega_menu__list">
+                    <a href="" class="mega_menu__link">Dresses</a>
+                  </li>
+                  <li class="mega_menu__list">
+                    <a href="" class="mega_menu__link">Tops</a>
+                  </li>
+                  <li class="mega_menu__list">
+                    <a href="" class="mega_menu__link">Sweaters/Knits</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="mega_menu__column">
+                <MenuWomanMini />
+                <img src="../../src/assets/imgs/mega_menu.png" alt="#" />
+              </div>
+            </div>
+          </li>
+          <li class="menu__list">
+            <a href="shoping_cart.html" class="menu__link">Women</a>
+          </li>
+          <li class="menu__list">
+            <a href="product.html" class="menu__link">Kids</a>
+          </li>
+          <li class="menu__list">
+            <a href="product.html" class="menu__link">Accoseriese</a>
+          </li>
+          <li class="menu__list">
+            <a href="product.html" class="menu__link">Featured</a>
+          </li>
+          <li class="menu__list">
+            <a href="product.html" class="menu__link">Hot Deals</a>
+            <div class="mega_menu mega_menu--left">
+              <div class="mega_menu__column">
+                <MenuWoman />
+              </div>
+              <div class="mega_menu__column">
+                <MenuWomanMini />
+                <h6 class="mega_menu__title mega_menu__title--middle">Women</h6>
+                <ul class="mega_menu__ul">
+                  <li class="mega_menu__list">
+                    <a href="" class="mega_menu__link">Dresses</a>
+                  </li>
+                  <li class="mega_menu__list">
+                    <a href="" class="mega_menu__link">Tops</a>
+                  </li>
+                  <li class="mega_menu__list">
+                    <a href="" class="mega_menu__link">Sweaters/Knits</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="mega_menu__column">
+                <MenuWomanMini />
+                <img src="../../src/assets/imgs/mega_menu.png" alt="#" />
+              </div>
+            </div>
+          </li>
+        </ul>
+      </nav>
+</template>
+
+<script>
+import MenuWoman from "../components/additional/MenuWoman.vue";
+import MenuWomanMini from "../components/additional/MenuWomanMini.vue";
+export default {
+  components: {
+    MenuWoman,
+    MenuWomanMini
+  }
+}
+</script>
+
+<style>
+
+</style>
